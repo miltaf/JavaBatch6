f06adc4af4e71c0c83dbacfd6eefeb05c41dbe31
Miltaf <mahboob.iltaf@gmail.com>
2020-03-20 01:06:23 -0400

    Thu Review Class of March 20

---
+++ src/com/syntax/class13/TwoDArraySum.java
@@ -0,0 +1,23 @@
+package com.syntax.class13;
+
+public class TwoDArraySum {
+	
+		public static void main(String[] args) {
+				int[][] num = { { 7, 1, 6, 12 }, { 9, 6, 2, 8 }, { 3, 0, 8, 5 } };
+
+				int[] sum = new int[num.length];
+
+				for (int row = 0; row < num.length; row++) {
+					for (int col = 0; col < num[row].length; col++) {
+						int element = num[row][col];
+						sum[row] += element;
+					}
+				}
+
+				// Let's iterate the sum array
+				for (int element : sum) {
+					System.out.println("Sum is " + element);
+				}
+
+			}
+		}
